ad6b60e0a88ed1ac4bec71d07b440d59fb5b45ae
tjeaneric <ericjohn415@gmail.com>
2022-11-13 16:22:08 +0200

    added error handling

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from models.location import Location
+from models.validation_error import ValidationError
 from services import openweather_service
 
 router = APIRouter()
@@ -8,8 +9,11 @@ router = APIRouter()
 
 @router.get("/api/weather/{city}")
 async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
-    report = await openweather_service.get_report_async(
-        loc.city, loc.state, loc.country, units
-    )
-
-    return report
+    try:
+        return await openweather_service.get_report_async(
+            loc.city, loc.state, loc.country, units
+        )
+    except ValidationError as ve:
+        return Response(content=ve.error_msg, status_code=ve.status_code)
+    except Exception as x:
+        return Response(content=str(x), status_code=500)
