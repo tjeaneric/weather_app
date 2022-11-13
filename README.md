900f4e94a585efc0b8ac1aa2533ba95b5deef513
tjeaneric <ericjohn415@gmail.com>
2022-11-13 15:35:37 +0200

    added caching

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather_app
+
+## This app was a part of the project in the course called modern apis with FastAPI
--- services/openweather_service.py
+++ services/openweather_service.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import httpx
+from infrastructure import weather_cache
 
 api_key: Optional[str] = None
 
@@ -7,6 +8,9 @@ api_key: Optional[str] = None
 async def get_report_async(
     city: str, state: Optional[str], country: str, units: str
 ) -> dict:
+    if forecast := weather_cache.get_weather(city, state, country, units):
+        return forecast
+
     if state:
         q = f"{city},{state},{country}"
     else:
@@ -20,4 +24,7 @@ async def get_report_async(
 
     data = resp.json()
     forecast = data["main"]
+
+    weather_cache.set_weather(city, state, country, units, forecast)
+
     return forecast
